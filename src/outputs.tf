918c0ac3b243d667bc7f78cdc2aa4f8d7a0135b8
Pavel Nagibin <pnagibin@gmail.com>
2023-09-18 21:35:33 +0300

    ter-homeworks-03

--- src/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_external_ip_address_netology-develop-platform-web" {
-value = yandex_compute_instance.platform.network_interface.0.nat_ip_address
-description = "vm external ip"
-}
-
-output "vm_external_ip_address_netology-develop-platform-db" {
-value = yandex_compute_instance.platform_db.network_interface.0.nat_ip_address
-description = "vm external ip"
-}
