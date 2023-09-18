952312dffd469d5c966b7d9c696dc1c768c4d84d
Pavel Nagibin <pnagibin@gmail.com>
2023-09-18 22:22:44 +0300

    ter-homeworks-03

--- src/count-vm.tf
+++ src/count-vm.tf
@@ -66,7 +66,6 @@ data "yandex_compute_image" "ubuntu" {
   family = var.vm_web_family
 }
 resource "yandex_compute_instance" "platform" {
-  security_group_id = "${data.yandex_vpc_security_group.example_dynamic.id}"
   count = 2
   name        = "${var.vm_web_name}-${var.vm_web_env}-${var.vm_web_project}-${var.vm_web_role}${count.index +1}"
   platform_id = var.vm_web_platform_id
@@ -84,6 +83,7 @@ resource "yandex_compute_instance" "platform" {
     preemptible = true
   }
   network_interface {
+    security_group_ids = yandex_vpc_security_group.example_dynamic.id
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
   }
