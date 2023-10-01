00d43d2ec896c70df401687974256ae0d9425861
Pavel Nagibin <pnagibin@gmail.com>
2023-10-01 21:18:56 +0300

    ter-homeworks-04

--- src/config.tf
+++
@@ -1,9 +0,0 @@
-data template_file "cloudinit" {
-  template = file("./cloud-init.yml")
-
-  vars = {
-    username           = var.username
-    ssh_public_key     = file(var.ssh_public_key)
-    packages           = jsonencode(var.packages)
-  }
-}
--- src/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-vpc_network_name = "develop"
-vpc_subnet_name = "develop-ru-central1-a"
-vpc_subnet_zone = "ru-central1-a"
-vpc_subnet_v4_cidr_blocks = "10.0.1.0/24"
